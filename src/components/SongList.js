08796c6e0ac52c79864a4e1e7ec49c0973e2d9b0
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-24 13:36:31 -0700

    continue working on structure

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,12 +2,13 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import propTypes from 'prop-types';
 import Song from "./Song";
 import { useFirestore } from 'react-redux-firebase';
 import firebase from 'firebase/app';
 import { message } from "antd";
 
-export default function SongList() {
+export default function SongList(props) {
   const [songList, setList] = useState(null);
   const [favePage, goToFaves] = useState(false);
   const [user, setUser] = useState(null);
@@ -23,38 +24,38 @@ export default function SongList() {
     setUser(auth.currentUser)
   }, [auth])
 
-  const onLike = (post) => {
-    let neededId = ''
-    let data = { liked: [] };
-    let haslikedPost = false;
-    firestore.collection("users").where("userId", "==", user.uid).get()
-      .then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-          neededId = doc.id
-          data = doc.data()
-        });
-        if (data.liked !== undefined) {
-          data.liked.forEach(likedPost => {
-            if (likedPost.songId === post.songId) {
-              haslikedPost = true;
-            }
-          })
-          if (haslikedPost) {
-            message.warn("Already liked this my dude!")
-          } else {
-            message.success("Post added to profile!")
-            return firestore.update({ collection: 'users', doc: neededId }, { liked: [...data.liked, post] })
+  // const onLike = (post) => {
+  //   let neededId = ''
+  //   let data = { liked: [] };
+  //   let haslikedPost = false;
+  //   firestore.collection("users").where("userId", "==", user.uid).get()
+  //     .then(function (querySnapshot) {
+  //       querySnapshot.forEach(function (doc) {
+  //         neededId = doc.id
+  //         data = doc.data()
+  //       });
+  //       if (data.liked !== undefined) {
+  //         data.liked.forEach(likedPost => {
+  //           if (likedPost.songId === post.songId) {
+  //             haslikedPost = true;
+  //           }
+  //         })
+  //         if (haslikedPost) {
+  //           message.warn("Already liked this my dude!")
+  //         } else {
+  //           message.success("Post added to profile!")
+  //           return firestore.update({ collection: 'users', doc: neededId }, { liked: [...data.liked, post] })
 
-          }
-        } else {
-          return firestore.update({ collection: 'users', doc: neededId }, { liked: [post] })
-        }
-      })
-      .catch(function (error) {
+  //         }
+  //       } else {
+  //         return firestore.update({ collection: 'users', doc: neededId }, { liked: [post] })
+  //       }
+  //     })
+  //     .catch(function (error) {
 
-        console.log("Error getting documents: ", error);
-      });
-  }
+  //       console.log("Error getting documents: ", error);
+  //     });
+  // }
 
   return (
 
