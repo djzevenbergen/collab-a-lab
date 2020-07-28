8a979f819a8d3c63aa3c7a21fb414e77eac2dd6e
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-28 12:31:46 -0700

    fix

--- src/components/MixerEditor.js
+++ src/components/MixerEditor.js
@@ -5,17 +5,54 @@ import { Slider, Switch } from "antd";
 
 const MixerEditor = (props) => {
   const { song } = props;
+
+
+
+  const settings = { vol1: 1, vol2: "3", vol3: "3", vol4: "3", vol5: "3", vol6: "3", vol7: "3", vol8: "3" }
+  const { vol1, vol2, vol3, vol4, vol5, vol6, vol7, vol8 } = settings;
+
   const [mixerOpen, openMixer] = useState(false);
-  const [track1Vol, changeTrack1Vol] = useState(50);
+  const [track1Vol, changeTrack1Vol] = useState(vol1);
+  const [track2Vol, changeTrack2Vol] = useState(vol1);
+  const [track3Vol, changeTrack3Vol] = useState(vol1);
+  const [track4Vol, changeTrack4Vol] = useState(vol1);
+  const [track5Vol, changeTrack5Vol] = useState(vol1);
+  const [track6Vol, changeTrack6Vol] = useState(vol1);
+  const [track7Vol, changeTrack7Vol] = useState(vol1);
+  const [track8Vol, changeTrack8Vol] = useState(vol1);
   const toggleModal = () => openMixer(!mixerOpen);
 
-
-
-
-
-  function update(e) {
+  function update1(e) {
     console.log(e);
     changeTrack1Vol(e);
+  }
+  function update2(e) {
+    console.log(e);
+    changeTrack2Vol(e);
+  }
+  function update3(e) {
+    console.log(e);
+    changeTrack3Vol(e);
+  }
+  function update4(e) {
+    console.log(e);
+    changeTrack4Vol(e);
+  }
+  function update5(e) {
+    console.log(e);
+    changeTrack5Vol(e);
+  }
+  function update6(e) {
+    console.log(e);
+    changeTrack6Vol(e);
+  }
+  function update7(e) {
+    console.log(e);
+    changeTrack7Vol(e);
+  }
+  function update8(e) {
+    console.log(e);
+    changeTrack8Vol(e);
   }
 
 
@@ -34,7 +71,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
-                  <Slider name="track1Slide" id="track1Slide" defaultValue={30} onChange={update} />
+                  <Slider name="track1Slide" id="track1Slide" defaultValue={vol1} onChange={update1} />
 
                   <p>{track1Vol}</p>
                 </Col>
@@ -46,6 +83,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track2Slide" id="track2Slide" defaultValue={vol2} onChange={update2} />
                 </Col>
               </CardBody>
             </Card>
@@ -55,6 +93,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track3Slide" id="track3Slide" defaultValue={vol3} onChange={update3} />
                 </Col>
               </CardBody>
             </Card>
@@ -64,6 +103,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track4Slide" id="track4Slide" defaultValue={vol4} onChange={update4} />
                 </Col>
               </CardBody>
             </Card>
@@ -73,6 +113,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track5Slide" id="track5Slide" defaultValue={vol5} onChange={update5} />
                 </Col>
               </CardBody>
             </Card>
@@ -82,6 +123,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track6Slide" id="track6Slide" defaultValue={vol6} onChange={update6} />
                 </Col>
               </CardBody>
             </Card>
@@ -91,6 +133,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track7Slide" id="track7Slide" defaultValue={vol7} onChange={update7} />
                 </Col>
               </CardBody>
             </Card>
@@ -100,6 +143,7 @@ const MixerEditor = (props) => {
                 <Col>
                   <Button className="mixer-play-button">Play</Button>
                   <Button className="mixer-play-button">Stop</Button>
+                  <Slider name="track8Slide" id="track8Slide" defaultValue={vol8} onChange={update8} />
                 </Col>
               </CardBody>
             </Card>
