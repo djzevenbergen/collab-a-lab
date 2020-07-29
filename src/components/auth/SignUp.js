5df47be558248dcc84c8f1862295964483dc99e2
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-29 11:01:23 -0700

    add unique usernames

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -11,52 +11,53 @@ function SignUp() {
   const [hidden, setHidden] = useState(true);
 
   const firestore = useFirestore();
-  function doSignUp(event) {
+  async function doSignUp(event) {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     const userName = event.target.userName.value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function (data) {
-      console.log(data.user.uid);
-      message.success("successfully signed up!");
-      setHidden(!hidden);
-      checkUsername(userName, data);
-      AddUserToDb(data, userName);
 
-    }).catch(function (error) {
-      console.log(error.message);
-    });
-  }
+    let userNameList = [];
+    await firestore.collection("usernames").where("username", "==", userName).get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          userNameList.push(doc.data().username);
+          console.log(doc.id);
+          console.log(doc.data().username);
 
 
-  const checkUsername = (uName, data) => {
-    try {
-      let userNameList = [];
-      firestore.collection("usernames").where("username", "==", uName).get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (doc) {
-            userNameList.push(doc.data().username);
-            console.log(doc.id);
-            console.log(doc.data().username);
-
-
-          })
         })
+      })
 
-      if (userNameList.includes(uName)) {
-        throw ("this username is take");
-      }
-      else {
-        return firestore.collection("usernames").add({ username: uName, userId: data.user.uid });
-      }
+    console.log(userNameList[0])
+    if (userNameList[0] != (userName)) {
 
 
-    } catch (error) {
-      message.error(error.message);
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function (data) {
+        console.log(data.user.uid);
+        message.success("successfully signed up!");
+        setHidden(!hidden);
+
+
+        AddUserToDb(data, userName);
+        AddUsername(data, userName)
+
+      }).catch(function (error) {
+        console.log(error.message);
+      });
+    } else {
+      message.error("username already taken");
     }
-
-
   }
+
+  const AddUsername = (data, uName) => {
+
+
+    return firestore.collection("usernames").add({ username: uName, userId: data.user.uid });
+  }
+
+
+
 
   const AddUserToDb = (data, userName) => {
     try {
